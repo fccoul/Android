421066b2ab024278a8be427a175412fe706b38f7
fccoul <fccoul@gmail.com>
2017-07-03 22:13:40 +0000

    Fragment swipe / tabs

--- FCOApplication_Back/app/src/main/java/com/example/fhcoulibaly/fcoapplication_back/FragmentHosted.java
+++ FCOApplication_Back/app/src/main/java/com/example/fhcoulibaly/fcoapplication_back/FragmentHosted.java
@@ -25,18 +25,22 @@ public class FragmentHosted extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_hosted);
 
+/*
 
-        /*
         SimplePagerAdapter adapter=new SimplePagerAdapter(getSupportFragmentManager());
         ViewPager pager=(ViewPager)findViewById(R.id.fragmentPager);
         pager.setAdapter(adapter);
-        */
+*/
 
 
         final ViewPager pager=(ViewPager)findViewById(R.id.fragmentPager);
 
         final ActionBar actionBar=getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        SimplePagerAdapter adapter=new SimplePagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+
          ActionBar.TabListener tabListener=new ActionBar.TabListener() {
              @Override
              public void onTabSelected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
@@ -69,9 +73,11 @@ public class FragmentHosted extends FragmentActivity {
             public void onPageSelected(int position)
             {
                 actionBar.setSelectedNavigationItem(position);
-               
+                Log.d(TAG,"index :"+position);
             }
         });
+
+
     }
 
 
